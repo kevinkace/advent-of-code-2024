cb7fa1d3603b055a393a5d9fef3ae9759ca205cf
Kevin Cameron <kevinacameron@hotmail.com>
2024-12-02 21:57:33 -0800

    template upate

--- scaffold-day/template/tests/day.spec.js
+++ scaffold-day/template/tests/day.spec.js
@@ -1,7 +1,7 @@
 import { describe, expect, test } from "vitest";
 
 import example1 from "../data/example-1";
-import part1 from "../data/01-1";
+import part1 from "../data/{day}-1";
 
 
 describe("day {day} - 1", () => {
