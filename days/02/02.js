0a5d236c0fc13055a439df7456706a5954cb5c07
Kevin Cameron <kevinacameron@hotmail.com>
2024-12-04 21:35:52 -0800

    just a bunch of garbage but working on it

--- days/02/02.js
+++ days/02/02.js
@@ -28,3 +28,138 @@ export function testSafe(data) {
         return false;
     });
 }
+
+function safeValues(currValue, nextValue, dir) {
+    // done! (watchout for 0)
+    if (nextValue === undefined) {
+        return true;
+    }
+
+    if (nextValue === currValue) {
+        return false;
+    }
+
+    const currDir = nextValue > currValue ? 1 : -1;
+
+    if (dir && currDir !== dir) {
+        return false;
+    }
+
+    if (
+        nextValue > (currValue * currDir) &&
+        Math.abs(nextValue - currValue) <= 3
+    ) {
+        return currDir;
+    }
+
+    return false;
+}
+
+export function testSafeTolerance(data, tolerance = 1) {
+    let dir;
+    let faults = 0;
+
+    for (let i = 0; i < data.length - 1; i++) {
+        for (let j = i+1; j < data.length; j++) {
+            let nextI = i + j;
+            let currDir;
+
+            currDir = safeValues(data[i], data[nextI], currDir);
+            console.log({currDir})
+
+            if (
+                currDir === false ||
+                dir && dir !== currDir
+            ) {
+                faults++;
+
+                continue;
+            }
+
+            if (currDir === true) {
+                break;
+            }
+
+            if (currDir === true) {
+                return faults <= tolerance;
+            }
+
+            dir === dir || currDir;
+        }
+
+        console.log("end of outer loop")
+    }
+
+    return false;
+}
+
+export function validateDiffs(diffs) {
+    let dir;
+
+    return diffs.every(diff => {
+        const currDir = diff > 0 ? 1 : -1;
+
+        if (dir && dir !== currDir || diff === 0) {
+            return false;
+        }
+
+        dir = currDir;
+
+        return Math.abs(diff) <= 3;
+    });
+}
+
+export function validateDiffsTolerance(diffs, tolerance = 1) {
+    let dir;
+    let faults = 0;
+
+
+    // i is not incremented here!
+    for (let i = 0; i < diffs.length; /* NOTHING */) {
+        const currDiff = diffs[i];
+        const currDir = currDiff > 0 ? 1 : -1;
+
+        const invalid = dir && dir !== currDir || currDiff === 0 || Math.abs(currDiff) > 3;
+
+        if (invalid) {
+            faults++;
+
+            diffs.splice(i, 1);
+            diffs[i] = currDiff + diffs[i];
+
+        } else {
+            i++;
+        }
+
+        if (faults > tolerance) {
+            return false;
+        }
+
+        dir = dir || currDir;
+    }
+
+    return true;
+}
+
+function getDir(diff) {
+    return diff > 0 ? 1 : -1;
+}
+
+export function checkDirection(diffs) {
+    if (diffs.join().includes("0")) {
+        return false;
+    }
+
+    let dir = getDir(diffs[0]);
+
+    return diffs.every(diff => (dir === getDir(diff)));
+}
+
+export function checkDistances(diffs, { max = 3 } = {}) {
+    return diffs.every(diff => Math.abs(diff) <= max);
+}
+
+
+export function valDiffs2099(diffs) {
+    return checkDirections(diffs) && checkDistances(diffs);
+}
--- days/02/tests/02.spec.js
+++ days/02/tests/02.spec.js
@@ -1,19 +1,23 @@
 import { describe, expect, test } from "vitest";
 
+import { parse2d } from "../../../utils/data-parsers";
+
 import example1 from "../data/example-1";
 import part1 from "../data/02-1";
-import { parse2d } from "../../../utils/data-parsers";
-import { testSafe } from "../02";
 
+import { testSafe, testSafeTolerance, validateDiffs, validateDiffsTolerance } from "../02";
+import { arrToDiffs } from "../../../utils/lists";
+
+const parsedExample = parse2d(example1);
+const parsedData = parse2d(part1);
 
 describe("day 02 - 1", () => {
     test("example", () => {
-        const data = parse2d(example1);
-        expect(data).toMatchSnapshot();
+        expect(parsedExample).toMatchSnapshot();
 
-        expect(testSafe(data[0])).toBe(true);
+        expect(testSafe(parsedExample[0])).toBe(true);
 
-        expect(data.map(testSafe)).toMatchSnapshot();
+        expect(parsedExample.map(testSafe)).toMatchSnapshot();
     });
 
     test("ad hoc", () => {
@@ -23,24 +27,78 @@ describe("day 02 - 1", () => {
     });
 
     test("part 1", () => {
-        const data = parse2d(part1);
+        const trueCount = parsedData.map(testSafe).filter(Boolean).length;
 
-        // data.forEach(r => {
-        //     console.log(r.join(" "), testSafe(r));
-        // });
-
-        const trueCount = data.map(testSafe).filter(Boolean).length;
-
-        console.log("trueCount", trueCount);
+        console.log({ trueCount });
 
         expect(trueCount).toBeGreaterThan(512);
     });
 });
 
 describe("day 02 - 2", () => {
-    test("example", () => {
+    test.skip("example", () => {
+        const trueCount = parsedExample.map(row => testSafeTolerance(row)).filter(Boolean).length;
+
+        expect(trueCount).toBe(4);
+    });
+
+    test("ad hoc - single row", () => {
+        const diffs4 = arrToDiffs(parsedExample[4]);
+
+        expect(diffs4).toMatchSnapshot();
+    });
+
+    test.only("ad hoc - pop first", () => {
+        const data = parsedData[24];
+        const diffs = arrToDiffs(data);
+
+        console.log({ data, diffs });
+        const valid = validateDiffsTolerance(diffs);
+
+        console.log({ valid, diffs, data });
+    });
+
+    test("ad hoc", () => {
+
+
+        // expect(validateDiffs(diffs)).toBe(false);
+
+        // const allDiffs = parsedExample.map(row => validateDiffs(arrToDiffs(row)));
+
+        // expect(allDiffs).toMatchSnapshot();
+
+
+        // expect(validateDiffsTolerance(arrToDiffs(parsedExample[3]))).toBe(true);
+
+        const allValidations = parsedExample.map(row => {
+            const diffs = arrToDiffs(row);
+
+            return validateDiffsTolerance(diffs);
+        });
+
+        expect(allValidations).toMatchSnapshot();
     });
 
     test("part 2", () => {
+        const allValidations = parsedData.map(row => {
+            const diffs = arrToDiffs(row);
+
+            return validateDiffsTolerance(diffs);
+        });
+
+        const trueCount = allValidations.filter(Boolean).length;
+
+        console.log({ trueCount });
+
+        console.table(allValidations.map((v, i) => {
+            if (!v) {
+                return { i, d : parsedData[i].join(","), diffs : arrToDiffs(parsedData[i]).join(",") };
+            }
+        }).filter(Boolean));
+
+        expect(trueCount).toBeGreaterThan(592);
+        expect(trueCount).toBeLessThan(800);
+
+        // expect(allValidations).toEqual();
     });
 });
