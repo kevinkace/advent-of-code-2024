a117ec4099dfc7c8e023a1beb122786f83c98eea
Kevin Cameron <kevinacameron@hotmail.com>
2024-12-07 10:58:36 -0800

    scaffold day 4

---
+++ days/04/readme.md
@@ -0,0 +1,17 @@
+# Day 04
+
+https://adventofcode.com/2024/day/04
+
+
+
+## Part 1
+
+
+
+## Part 2
+
+
+
+## Closing
+
+
---
+++ days/04/tests/04.spec.js
@@ -0,0 +1,35 @@
+import { readFileSync } from "node:fs";
+import path             from "node:path";
+
+import { describe, expect, test } from "vitest";
+
+function getFile(filePath) {
+    return  readFileSync(path.join(__dirname, filePath), "utf-8");
+}
+
+const example1 = getFile("../data/example-1.log");
+const part1    = getFile("../data/04-1.log");
+
+const answers = {
+    // example1:
+    // part1: ,
+    // part2:,
+    // example2
+};
+
+
+describe("day 04 - 1", () => {
+    test("example", () => {
+    });
+
+    test("part 1", () => {
+    });
+});
+
+describe("day 04 - 2", () => {
+    test("example", () => {
+    });
+
+    test("part 2", () => {
+    });
+});
