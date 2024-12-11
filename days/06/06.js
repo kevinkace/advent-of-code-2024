274ee2fe97ba50e66bca81d94aebcc50737645e2
Kevin Cameron <kevinacameron@hotmail.com>
2024-12-10 22:09:22 -0800

    solution for example

--- days/06/06.js
+++ days/06/06.js
@@ -1,1 +1,127 @@
-// see tests
+import { writeFileSync } from "fs";
+import { get }           from "http";
+
+const EOL = require("os").EOL;
+
+export function getDim(map) {
+    const rows = map.split(EOL);
+
+    return {
+        rows : rows.length,
+        cols : rows[0].length
+    };
+}
+
+export function charAt(map, { x, y }) {
+    const { rows } = getDim(map);
+
+    return map.replaceAll(EOL, "").charAt(y * rows + x);
+}
+
+export function setChar(map, { x, y }, char) {
+    const rows = map.split(EOL);
+
+    rows[y] = `${rows[y].substring(0, x)}${char}${rows[y].substring(x + 1)}`;
+
+    return rows.join(EOL);
+}
+
+export function inBounds(map, { x, y }) {
+    const { rows, cols } = getDim(map);
+
+    return x >= 0 && x < cols && y >= 0 && y < rows;
+}
+
+/**
+ * Rotate a direction by 90 degrees.
+ * @param {Object} delta - The current direction
+ * @param {Number} delta.dx - The x component of the direction
+ * @param {Number} delta.dy - The y component of the direction
+ * @param {"CW"|"CCW"} rotation - The rotation direction
+ * @returns {Object} The new direction
+ */
+export function rotateDirection({ dx, dy }, rotation) {
+    return rotation === "CCW" ? { dx : dy + 0, dy : -dx + 0 } : { dx : -dy + 0, dy : dx };
+}
+
+export function move(map, { x, y, dx, dy }) {
+    const newPosition = { x : x + dx, y : y + dy };
+
+    // if (!inBounds(map, newPosition)) {
+    //     return true;
+    // }
+
+    if (charAt(map, newPosition) === "#") {
+        const newDelta = rotateDirection({ dx, dy }, "CW");
+
+        return move(map, { x, y, ...newDelta });
+    }
+
+    return { ...newPosition, dx, dy };
+}
+
+export function getStartPosition(map, startChar) {
+    const { rows, cols } = getDim(map);
+
+    const idx = map.replaceAll(EOL, "").indexOf(startChar);
+
+    return { x : idx % cols, y : Math.floor(idx / rows) };
+}
+
+export function walkMap(map, { startChar = "^", dx = 0, dy = -1 } = {}) {
+    let currPosition = { ...getStartPosition(map, startChar), dx, dy };
+    let moves        = 0;
+    let newMap       = map.slice(0);
+
+    const maps = [ newMap ];
+
+    while (inBounds(map, currPosition)) {
+        if (moves !== 0) {
+            // maps.push(setChar(maps.at(-1), currPosition, "X"));
+            newMap = setChar(newMap, currPosition, "X");
+
+            writeFileSync("./map.txt", newMap);
+        }
+
+        currPosition = move(map, currPosition);
+
+        moves++;
+    }
+
+    // console.log(maps, currPosition);
+
+    return { moves, positions : newMap.replaceAll(EOL, "").split("").filter((char) => char === "X").length };
+}
+
+export function countInfections(map, iterations) {
+    const { rows, cols } = getDim(map);
+    const start          = { x : Math.floor(cols / 2), y : Math.floor(rows / 2) };
+    const startDirection = { dx : 0, dy : -1 };
+    let position         = start;
+    let direction        = startDirection;
+    let infections       = 0;
+
+    for (let i = 0; i < iterations; i++) {
+        const char = charAt(map, position);
+
+        if (char === "#") {
+            direction = rotateDirection(direction, "CW");
+            map       = `${map.substring(0, position.y * cols + position.x)}.${map.substring(position.y * cols + position.x + 1)}`;
+        } else {
+            direction = rotateDirection(direction, "CCW");
+            map       = `${map.substring(0, position.y * cols + position.x)}#${map.substring(position.y * cols + position.x + 1)}`;
+            infections++;
+        }
+
+        const result = move(map, position, direction);
+
+        if (result === true) {
+            break;
+        }
+
+        position  = result.position;
+        direction = result.direction;
+    }
+
+    return infections;
+}
--- days/06/tests/06.spec.js
+++ days/06/tests/06.spec.js
@@ -1,17 +1,18 @@
 import { readFileSync } from "node:fs";
 import path             from "node:path";
 
-import { describe, expect, test } from "vitest";
+import { describe, expect, test }                     from "vitest";
+import { getDim, inBounds, rotateDirection, walkMap } from "../06";
 
 function getFile(filePath) {
     return readFileSync(path.join(__dirname, filePath), "utf-8");
 }
 
-const example1 = getFile("../data/example-1.log");
-const part1    = getFile("../data/part-1.log");
+const example1 = getFile("../data/example-1.txt");
+const part1    = getFile("../data/input-1.txt");
 
 const answers = {
-    // example1:
+    example1 : 41
     // part1: ,
     // part2:,
     // example2
@@ -19,12 +20,35 @@ const answers = {
 
 
 describe("day 06 - 1", () => {
-    test("example", () => {
-
+    test("getDim", () => {
+        expect(getDim(example1)).toEqual({ rows : 10, cols : 10 });
     });
 
-    // test("part 1", () => {
-    // });
+    test("inBounds", () => {
+        expect(inBounds(example1, { x : 0, y : 0 })).toBe(true);
+        expect(inBounds(example1, { x : 9, y : 9 })).toBe(true);
+        expect(inBounds(example1, { x : 10, y : 10 })).toBe(false);
+        expect(inBounds(example1, { x : -1, y : 9 })).toBe(false);
+    });
+
+    test("rotateDirection", () => {
+        expect(rotateDirection({ dx : 0, dy : -1 }, "CW")).toEqual({ dx : 1, dy : 0 });
+        expect(rotateDirection({ dx : 0, dy : -1 }, "CCW")).toEqual({ dx : -1, dy : 0 });
+        expect(rotateDirection({ dx : 1, dy : 0 }, "CW")).toEqual({ dx : 0, dy : 1 });
+        expect(rotateDirection({ dx : 1, dy : 0 }, "CCW")).toEqual({ dx : 0, dy : -1 });
+    });
+
+    test("example", () => {
+        expect(walkMap(example1).positions).toBe(answers.example1);
+    });
+
+    test.only("part 1", () => {
+        const { positions } = walkMap(part1);
+
+        console.log(positions);
+
+        expect(positions).toBeGreaterThan(5211);
+    });
 });
 
 // describe("day 06 - 2", () => {
