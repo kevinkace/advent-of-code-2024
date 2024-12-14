246d4b29f3b13a84d64dc36309bacb1294d147a1
Kevin Cameron <kevinacameron@hotmail.com>
2024-12-14 10:56:28 -0500

    part 1

--- days/07/07.js
+++ days/07/07.js
@@ -1,1 +1,44 @@
-// see tests
+const eol = require("os").EOL;
+
+export function parseRow(row) {
+    const [ solution, ...values ] = row.split(/\:? /).map(Number);
+
+    return { solution, values };
+}
+
+export function walkRows(data, cb) {
+    data.split(eol).forEach(cb);
+}
+
+export function binaryCounter(bin) {
+    const binary = parseInt(bin, 2) + 1;
+
+    return binary.toString(2).padStart(bin.length, "0");
+}
+
+export function calcFromBin(values, bin) {
+    return values.reduce((acc, number, i) => {
+        if (!i) {
+            return number;
+        }
+
+        return bin[i - 1] === "0" ? acc + number : acc * number;
+    }, 0);
+}
+
+export function testOps({ solution, values }) {
+    const iterations = Math.pow(2, values.length);
+
+    let binary = "0".repeat(values.length - 1);
+
+
+    for (let i = 0; i < iterations; i++) {
+        if (calcFromBin(values, binary) === solution) {
+            return true;
+        }
+
+        binary = binaryCounter(binary);
+    }
+
+    return false;
+}
--- days/07/tests/07.spec.js
+++ days/07/tests/07.spec.js
@@ -3,6 +3,8 @@ import path             from "node:path";
 
 import { describe, expect, test } from "vitest";
 
+import { binaryCounter, calcFromBin, parseRow, testOps, walkRows } from "../07";
+
 function getFile(filePath) {
     return readFileSync(path.join(__dirname, filePath), "utf-8");
 }
@@ -11,20 +13,66 @@ const example1 = getFile("../data/example-1.txt");
 const part1    = getFile("../data/input-1.txt");
 
 const answers = {
-    // example1:
-    // part1: ,
+    example1 : 3749,
+    part1    : 975671981569,
     // part2:,
     // example2
 };
 
 
 describe("day 07 - 1", () => {
-    test("example", () => {
+    test("parse and walk row", () => {
+        walkRows(example1, (row) => {
+            const { solution, values } = parseRow(row);
 
+            expect(typeof solution).toBe("number");
+            expect(Array.isArray(values)).toBe(true);
+        });
     });
 
-    // test("part 1", () => {
-    // });
+    test("getOps", () => {
+        expect(binaryCounter("0000")).toBe("0001");
+        expect(binaryCounter("0101")).toBe("0110");
+    });
+
+    test("calcFromBin", () => {
+        expect(calcFromBin([ 10, 19 ], "1")).toBe(190);
+        expect(calcFromBin([ 10, 19 ], "0")).toBe(29);
+    });
+
+    test("testOps", () => {
+        expect(testOps({ solution : 190, values : [ 10, 19 ] })).toBe(true);
+        expect(testOps({ solution : 29, values : [ 10, 19 ] })).toBe(true);
+        expect(testOps({ solution : 29, values : [ 10, 18 ] })).toBe(false);
+    });
+
+    test("example", () => {
+        let sum = 0;
+
+        walkRows(example1, (row) => {
+            const { solution, values } = parseRow(row);
+
+            if (testOps({ solution, values })) {
+                sum += solution;
+            }
+        });
+
+        expect(sum).toBe(answers.example1);
+    });
+
+    test("part 1", () => {
+        let sum = 0;
+
+        walkRows(part1, (row) => {
+            const { solution, values } = parseRow(row);
+
+            if (testOps({ solution, values })) {
+                sum += solution;
+            }
+        });
+
+        expect(sum).toBe(answers.part1);
+    });
 });
 
 // describe("day 07 - 2", () => {
